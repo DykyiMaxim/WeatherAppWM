59402e2b035e7f1e4d7c7a7bc73fb60873f1e8b8
Dykyi Maksym <maxdykii468@gmail.com>
2022-07-14 21:43:50 +0200

    Setuping dagger hilt

---
+++ app/src/main/java/com/plcoding/weatherapp/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.plcoding.weatherapp.di
+
+import android.app.Application
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.plcoding.weatherapp.data.remote.WeatherAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun privideWeatherApi():WeatherAPI{
+        return Retrofit.Builder()
+            .baseUrl("https://api.open-meteo.com/")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+    @Provides
+    @Singleton
+    fun providerFusedLocationProviderClient(app:Application):FusedLocationProviderClient{
+        return LocationServices.getFusedLocationProviderClient(app)
+    }
+}
