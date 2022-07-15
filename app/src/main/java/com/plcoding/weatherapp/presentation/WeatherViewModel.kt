75c64c8da97d4d69673b5902949fc8d18f7fba69
Dykyi Maksym <maxdykii468@gmail.com>
2022-07-15 23:04:08 +0200

    Set up hilt start working on ViewModel

---
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherViewModel.kt
@@ -0,0 +1,39 @@
+package com.plcoding.weatherapp.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.weatherapp.domain.location.LocationTreacker
+import com.plcoding.weatherapp.domain.repository.WeatherRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WeatherViewModel @Inject constructor(
+    private val repository:WeatherRepository,
+    private val locationTreacker: LocationTreacker
+
+):ViewModel() {
+
+    var state by mutableStateOf(WeatherState())
+    private set
+
+    fun loadWeatherInfo(){
+        viewModelScope.launch {
+            state = state.copy(
+                isLoading = true,
+                error = null
+            )
+            locationTreacker.getCurrLocation()?.let{ location ->
+                when(val result = repository.getWeatherData(location.latitude, location.longitude)){
+
+                }
+
+            }
+
+        }
+    }
+}
