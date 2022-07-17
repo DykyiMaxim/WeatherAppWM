eccb1eec409be5cb2292fbfda6bc13e3dca160ef
Dykyi Maksym <maxdykii468@gmail.com>
2022-07-17 23:15:04 +0100

    Working on Compose UI

--- app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
@@ -1,15 +1,54 @@
 package com.plcoding.weatherapp.presentation
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.plcoding.weatherapp.presentation.ui.theme.DarkBlue
+import com.plcoding.weatherapp.presentation.ui.theme.DeepBlue
 import com.plcoding.weatherapp.presentation.ui.theme.WeatherWMTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel:WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions() //already checked in location tracker
+        ){
+            viewModel.loadWeatherInfo()
+
+        }
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+
+        ))
         setContent {
             WeatherWMTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(DarkBlue)
+                ) {
+                    WeatherCard(
+                        state = viewModel.state,
+                        backgroundColor = DeepBlue )
+
+                }
                 
             }
         }
