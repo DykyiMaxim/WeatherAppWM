f61cb9babbc67d62d9f904c7b342b03e570c37ca
Dykyi Maksym <108894931+DykyiMaxim@users.noreply.github.com>
2022-07-21 19:35:31 +0300

    Finished the app

--- app/src/main/java/com/wmdykyi/weatherapp/domain/repository/WeatherRepository.kt
+++ app/src/main/java/com/wmdykyi/weatherapp/domain/repository/WeatherRepository.kt
@@ -4,5 +4,5 @@ import com.wmDykyi.weatherapp.domain.util.Resource
 import com.wmDykyi.weatherapp.domain.weather.WeatherInfo
 
 interface WeatherRepository {
-    suspend fun getWeatherData(lat: Double, long:Double):Resource<WeatherInfo>
+    suspend fun getWeatherData(lat: Double, long:Double): Resource<WeatherInfo>
 }
